f7e6712763902477cf14bfb3018bdde4b6cf3957
Derrick Strong <derrickmstrong@gmail.com>
2020-06-01 10:00:56 -0500

    Temporarily removed audioVolume function

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ $(function () {
       return;
     } else {
       // Set audio volume
-      audioVolume(0.1);
+      // audioVolume(0.1);
       $('#typewriter').trigger('play');
       $('#andre3000').trigger('play');
     }
